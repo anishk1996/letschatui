a56a1a98cd0c8197ea028841cfefe0a0587c1a2a
Anish Kumar <kr.anish96@gmail.com>
2023-06-13 13:08:33 +0530

    env fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    endpoint: 'https://letschatservice.onrender.com/',
+    endpoint: 'https://letschatservice.onrender.com',
     commentedendpoint: 'https://letchatservice.vercel.app'
   };
